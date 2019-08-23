cb4f954cdd30ee7dc3fa0df89b8fb94e8476534b
DimaSmile <efremov2293@gmail.com>
2019-08-23 17:26:59 +0300

    migrate routes folder 5.7->5.8

--- web/routes/web.php
+++ web/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,28 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function(){
+    Route::resource('posts', 'PostController')->names('blog.posts');
+});
+
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog'
+];
+
+Route::group($groupData, function(){
+    //BlogCategory
+    $methods = ['index', 'edit', 'update', 'create', 'store'];
+    Route::resource('categories', 'CategoryController')
+    ->only($methods)
+    ->names('blog.admin.categories');
+
+    //BlogPost
+    Route::resource('posts', 'PostController')
+        ->except(['show'])
+        ->names('blog.admin.posts');
+});
+
+//Route::resource('rest', 'RestTestController')->names('restTest');
